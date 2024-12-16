54372943e7f22eec17c3371f00c738fd5553da21
David-Sienz <david.sienz@outlook.com>
2024-12-16 15:12:59 +0100

    finished 02_JS_Strings&Zahlen

---
+++ Javascript/02_JS_Strings_&_Zahlen/script.js
@@ -0,0 +1,10 @@
+//	1) Berechne das Ergebnis von 17.4 / 3.9. Gib folgenden Satz aus:
+let a = 17.4;
+let b = 3.9;
+let result = a/b;
+console.log("The result of " + 17.4 + " divided by " + 3.9 + " is " + result);
+
+// 2) Lege eine Variable firstname und eine Varialbe lastname an. Weise den Variablen Deinen Namen zu. Gib folgendes aus: 
+let firstname = "DAVID";
+let lastname = "Sienz";
+console.log("Ich heiße " + firstname + lastname);
