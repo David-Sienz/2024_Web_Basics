0fe78c3d4277ddbcc9382e3dacc2fc8f55bd53e2
David-Sienz <david.sienz@outlook.com>
2025-01-10 09:07:40 +0100

    finished 05_If_Bedingungen

---
+++ Javascript/05_If_Bedingungen/script.js
@@ -0,0 +1,18 @@
+let number = 17;
+//Aufgabe 1
+if (number < 20) {
+  console.log("Mini");
+} else if (number > 20 && number < 50) {
+  console.log("Medium");
+} else {
+  console.log("Large");
+}
+
+//Aufgabe 2
+if (number <= 7) {
+  console.log("Mini");
+} else if (number > 7 && number <= 90) {
+  console.log("Medium");
+} else {
+  console.log("Large");
+}
