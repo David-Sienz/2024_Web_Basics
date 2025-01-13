abf613a5aab73460331b781f3d7227c1ee7f835a
David-Sienz <david.sienz@outlook.com>
2025-01-13 15:46:49 +0100

    started 10_Funktionen

---
+++ Javascript/10_Funktionen/script.js
@@ -0,0 +1,42 @@
+// Rechnung 1
+function add(a, b) {
+  return a + b;
+}
+
+let result1 = add(5, 3);
+console.log(result1);
+
+// Rechnung 2
+function subtract(a, b) {
+  return a - b;
+}
+
+let result2 = subtract(8, 2);
+console.log(result2);
+
+// Rechnung 3
+function multiply(a, b) {
+  return a * b;
+}
+
+let result3 = multiply(3, 5);
+console.log(result3);
+
+// Rechnung 4
+function divide(a, b) {
+  return a / b;
+}
+
+let result4 = divide(10, 2);
+console.log(result4);
+
+// Rechnung 5
+function supercalculation(a, b) {
+  return ((a + b) / 2) * a;
+}
+
+let result5 = supercalculation(5, 3);
+console.log(result5);
+
+// Rechnung 6
+function printEasterDate(year) {}
