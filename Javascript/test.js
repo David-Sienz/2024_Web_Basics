ff4ed223eefcf216701e18d7d4b072d861612008
David-Sienz <david.sienz@outlook.com>
2025-01-31 09:10:35 +0100

    finished Doroteas Test

---
+++ Javascript/test.js
@@ -0,0 +1,34 @@
+//eins
+let sum = 0;
+for (var i = 7; i < 344; i++) {
+  if (i % 12 === 0) {
+    sum += 3 * i;
+  } else {
+    sum += i;
+  }
+}
+
+console.log(sum);
+
+//zwei
+let data = "Super duper";
+let result = "";
+
+for (let i = 0; i < data.length; i++) {
+  if (data[i] === "u" || data[i] === "e") {
+    result += "x";
+  } else {
+    result += data[i];
+  }
+}
+
+console.log(result);
+
+//drei
+let number = 100;
+while (number >= 25) {
+  if (number != 50 && number != 40 && number != 30) {
+    console.log(number);
+  }
+  number -= 5;
+}
