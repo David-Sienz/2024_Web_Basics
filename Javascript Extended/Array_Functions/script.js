6f32d8eaf9cc17e25ad285a1b735c463ed20e702
David-Sienz <david.sienz@outlook.com>
2025-03-07 10:38:27 +0100

    worked on array functions

---
+++ Javascript Extended/Array_Functions/script.js
@@ -0,0 +1,20 @@
+// //map
+// const items = [
+//   { name: "Bike", price: 100 },
+//   { name: "Car", price: 5000 },
+//   { name: "Shirt", price: 25 },
+//   { name: "Socks", price: 10 },
+//   { name: "Shoes", price: 50 },
+//   { name: "Watch", price: 150 },
+//   { name: "Jacket", price: 1000 },
+//   { name: "Coat", price: 1200 },
+//   { name: "Dress", price: 750 },
+//   { name: "Scarf", price: 10 },
+// ];
+
+// const filteredItems = items.filter((item) => {
+//   return item.price <= 100;
+// });
+
+// console.log(items);
+// console.log(filteredItems);
