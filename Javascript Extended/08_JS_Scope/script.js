3ff5d0dd28cdf29d6663eac778b2a02c63c94cb0
David-Sienz <david.sienz@outlook.com>
2025-03-07 10:06:09 +0100

    watched video "Differences Between Var, Let, and Const"

---
+++ Javascript Extended/08_JS_Scope/script.js
@@ -0,0 +1,39 @@
+/*if (true) {
+  var varVariable = "This is true";
+}
+console.log(varVariable);
+
+if (true) {
+  let letVariable = "This is true";
+}
+console.log(letVariable);
+*/
+
+/*console.log(varVariable2);
+
+var varVariable2 = "This is a variable";
+
+console.log(varVariable2);
+*/
+
+/*console.log(letVariable2);
+
+let letVariable2 = "This is a variable";
+
+console.log(letVariable2);
+*/
+
+// const constVar = 1;
+// let letVar = 1;
+
+// letVar = 2;
+// constVar = 2;
+
+// console.log(constVar);
+
+// const constVar = { name: "Bob" };
+// let letVar = 1;
+
+// constVar.name = "sally";
+
+// console.log(constVar);
